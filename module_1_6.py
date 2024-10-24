04091ab56328f71da4f8f3ef7fb014df701be6bd
NAT-G44 <gr.natasha.2003@gmail.com>
2024-10-24 23:11:41 +0800

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+all_homework = 12
+all_hours = 1.5
+name_course = "Pyton,"
+time_one = 1.5/12
+print("Курс:",name_course,"всего задач:",all_homework,"затрачено часов:",all_hours,"среднее время выполнения", time_one,"часа")
